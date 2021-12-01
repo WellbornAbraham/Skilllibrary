d6889c08b7d5e745f45da96dd7659d11a585d195
WellbornAbraham <WellbornAbraham@gmail.com>
2021-12-01 15:27:16 +0100

    Initial Commit

---
+++ scripts/server.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Black        0;30
+# Dark Gray     1;30
+# Red          0;31
+# Light Red     1;31
+# Green        0;32
+# Light Green   1;32
+# Brown/Orange 0;33
+# Yellow        1;33
+# Blue         0;34
+# Light Blue    1;34
+# Purple       0;35
+# Light Purple  1;35
+# Cyan         0;36
+# Light Cyan    1;36
+# Light Gray   0;37
+# White         1;37
+BLACK='\033[0;30m'
+DGRAY='\033[1;30m'
+RED='\033[0;31m'
+LRED='\033[1;31m'
+GREEN='\033[0;32m'
+LGREEN='\033[1;32m'
+ORANGE='\033[0;33m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;34m'
+LBLUE='\033[1;34m'
+PURPLE='\033[0;35m'
+LPURPLE='\033[1;35m'
+CYAN='\033[0;36m'
+LCYAN='\033[1;36m'
+LGRAY='\033[0;37m'
+WHITE='\033[1;37m'
+NC='\033[0m' # No Color
+
+cd ~
+echo -e "${GREEN}server.sh ${NC}started"
+cd ~/storage/shared/skilllibrary/website
+python -m http.server 8080
+echo -e "${GREEN}done ${NC}"
